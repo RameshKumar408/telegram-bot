8ca599b70c6a4f2f29abac67f753bd16551e767f
ramesh6435 <ckramesh0006@gmail.com>
2024-08-21 08:23:02 +0530

    changes

--- app/api/bot/route.js
+++ app/api/bot/route.js
@@ -3,6 +3,7 @@ export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
 
 import { Bot, webhookCallback } from 'grammy'
+const { Markup } = require('telegraf');
 
 const token = "7352687069:AAE_e2DG-X55FaeKa061I7y3_WCYJCjyHRU"
 
@@ -23,11 +24,9 @@ bot.api.setMyCommands([
 ]);
 
 bot.command("start", (ctx) => {
-    ctx.reply('Welcome! Click the button below to launch the mini app:', {
-        reply_markup: new InlineKeyboard()
-            .text('Launch App', 'https://example.com')
-            .callback('Callback Button', 'callback_data')
-    })
+    ctx.reply('Welcome! Click the button below to launch the mini app:', Markup.inlineKeyboard([
+        [Markup.button.url('Launch Application', "https://t.me/rameshfoodorderbot/rameshfoodorder")]
+    ]))
 });
 
 export const POST = webhookCallback(bot, 'std/http')
