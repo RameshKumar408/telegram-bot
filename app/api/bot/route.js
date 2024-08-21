7140fc190c8612f884c7c4a036b7a11d8245ddf0
ramesh6435 <ckramesh0006@gmail.com>
2024-08-21 07:38:21 +0530

    start

--- app/api/bot/route.js
+++ app/api/bot/route.js
@@ -9,8 +9,6 @@ const token = "7352687069:AAE_e2DG-X55FaeKa061I7y3_WCYJCjyHRU"
 if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
 
 const bot = new Bot(token)
-bot.on('message:text', async (ctx) => {
-    await ctx.reply(ctx.message.text)
-})
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
 export const POST = webhookCallback(bot, 'std/http')
